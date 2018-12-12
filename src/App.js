065da6bc03ae5b522d4ee004b81dec7f5044f058
baby7305 <songhaibei7305@163.com>
2018-12-12 22:52:29 +0800

    暂时URL Parameters和query string单参数测试

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello, world!</h1>
+      <Router>
+        <div>
+          <h2>Accounts</h2>
+          <ul>
+            <li>
+              <Link to={{ pathname: "/account", search: "?name=netflix" }}>
+                Netflix
+            </Link>
+            </li>
+            <li>
+              <Link to={{ pathname: "/basic", search: "?name=zillow-group" }}>
+                Zillow Group
+            </Link>
+            </li>
+            <li>
+              <Link to={{ pathname: "/chart", search: "?name=yahoo" }}>
+                Yahoo
+            </Link>
+            </li>
+            <li>
+              <Link to={{ pathname: "/delicious", search: "?name=modus-create" }}>
+                Modus Create
+            </Link>
+            </li>
+          </ul>
+          <Switch>
+            {/* <Route path='/:id' component={Child} /> */}
+            {/* <Route path='/:id' component={Child1} /> */}
+            <Route path="/child1" component={Child1} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
+
+const Child = ({ match }) => console.log('match', match) || (
+  <div>
+    <h3>query string</h3>
+    <h3>{match.params.id}</h3>
+  </div>
+)
+
+const Child1 = ({ location }) => console.log('location', location) || (
+  <div>
+    <h3>query string</h3>
+    <h3>{location.search}</h3>
+  </div>
+)
 
 export default App;

980761bcfb949b99aa588e84c67cc6ec27975ed6
baby7305 <songhaibei7305@163.com>
2018-12-12 15:20:31 +0800

    const、class调整一下写法，其实相互间可以替代

--- src/App.js
+++ src/App.js
@@ -2,32 +2,51 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
+class Index extends Component {
+  render() {
+    return (
+      <h2>Home</h2>
+    );
+  }
+}
 
-const AppRouter = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-          <li>
-            <Link to="/users/">Users</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Route path="/" exact component={Index} />
-      <Route path="/about/" component={About} />
-      <Route path="/users/" component={Users} />
-    </div>
-  </Router>
+const About = () => (
+  <div>
+    <h2> About </h2>
+  </div>
+);
+const Users = () => (
+  <div>
+    <h2> Users </h2>
+  </div>
 );
 
-export default AppRouter;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
